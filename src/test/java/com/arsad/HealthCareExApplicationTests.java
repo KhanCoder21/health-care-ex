dd9d42bd5af70e7aa0cd74e47c16883412efeb3b
Arsad <ayush.aak1998@gmail.com>
2023-03-18 00:28:32 +0530

    "Initial commit"

---
+++ src/test/java/com/arsad/HealthCareExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
